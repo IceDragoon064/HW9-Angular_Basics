ef14117fee07e6fed39f65f8617818823db74447
Leon Lu <ltlu1993@gmail.com>
2022-06-07 20:56:58 -0400

    Exercise 5

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 
-
-
-
 export class PlayingCard{
   cardVal: string;
   suit: string;
 }
+
 @Component({
   selector: 'app-root',
-  template: '<h1>Hello world {{title}}<br> {{card.cardVal}} of {{card.suit}}. </h1>',
+  template: `
+    <h1>Hello world {{title}}
+    <br>
+    {{card.cardVal}} of {{card.suit}}.
+    {{card1.cardVal}} of {{card1.suit}}
+    </h1>
+    Card: <input [(ngModel)]="card.cardVal">`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
@@ -19,4 +23,9 @@ export class AppComponent {
     cardVal: "Ace",
     suit: "Spades"
   };
+
+  card1: PlayingCard = {
+    cardVal: "Queen",
+    suit: "Diamonds"
+    };
 }
