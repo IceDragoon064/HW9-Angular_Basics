88730b078dc2cf0a285af04ead0e8152fe9f5139
Leon Lu <ltlu1993@gmail.com>
2022-06-07 20:44:45 -0400

    Debug exercises

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+
+
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,5 +11,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myapp';
-  lastName = 'lu';
 }
