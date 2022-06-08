6492b527213c5dec700eb11dbb183aa6d2796985
Leon Lu <ltlu1993@gmail.com>
2022-06-07 21:48:33 -0400

    Exercise 10

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -1,36 +1,33 @@
 import { Component } from '@angular/core';
-
-export class PlayingCard{
-  cardVal: string;
-  suit: string;
+export class PlayingCard {
+cardVal: string;
+suit: string;
 }
-
 @Component({
-  selector: 'app-root',
-  template: `
-    <input [(ngModel)]="myinput">
-    <input type='button' (click)=doSomething(myinput)>
-    {{myoutput}}
-  <br>
-    <input [(ngModel)]="myinput1">
-    <input type='button' (click)=doSomething1(myinput1)>
-    {{myoutput1}}
-    `,
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  myinput = '';
-  myoutput = '';
-  myinput1 = '';
-  myoutput1 = '';
-
-  doSomething(someContent){
-      alert(someContent);
-      this.myoutput = someContent;
+selector: 'app-root',
+// Multi-line content allowed with back ticks.
+  template: `<h1>Hello world! {{title}} <br/>
+  <!-- Show cards in unordered list. -->
+  <ul><li *ngFor="let card of cards" (click)="onSelect(card)">
+  {{card.cardVal}}</li>
+  </ul>
+  <div *ngIf="selectedCard">
+  <h2>{{selectedCard.cardVal}} **</h2>
+  <input [(ngModel)]="selectedCard.cardVal" placeholder="name"/>
+  </div>
+  `
+  })
+  export class AppComponent {
+  title = 'This is Angular!';
+  // Include card data in class.
+  cards:PlayingCard[] = [
+  { cardVal:"Ace", suit:"Spades"},
+  { cardVal:"Two", suit:"Clubs" },
+  { cardVal:"Six", suit:"Hearts"},
+  ];
+  selectedCard: PlayingCard;
+  onSelect(card: PlayingCard) {
+    alert(card.suit);
+  this.selectedCard = card;
   }
-
-  doSomething1(someContent1){
-    alert(someContent1);
-    this.myoutput1 = someContent1;
-}
-}
+  }
